2cc50caf78a324618896ec901821a25bda3b2063
Sukh Singh <sukh@orbs.com>
2024-10-03 15:01:47 +0100

    fix logo path

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <div className="p-4 flex w-full justify-between items-center fixed dark:bg-slate-950 ">
       <div className="flex gap-2 items-center">
-        <img src="/orbslogo.svg" className="w-6 h-6" />
+        <img src="/dex-playground/orbslogo.svg" className="w-6 h-6" />
         <span>Orbs Demo</span>
       </div>
       <div className="flex gap-4">
