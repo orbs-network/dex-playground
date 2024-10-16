f116cac642f7a3dc657d654c8692c09a95c8093f
denis-orbs <denis@orbs.com>
2024-10-16 12:06:47 +0300

    a

--- src/trade/twap/useTwapSdk.ts
+++
@@ -1,6 +0,0 @@
-import { useMemo } from 'react'
-import { constructSDK, Configs } from '@orbs-network/twap-sdk'
-
-export function useTwapSdk() {
-  return useMemo(() => constructSDK({ config: Configs.QuickSwap }), [])
-}
